cb25a6c00742b24be327ed43f47b22ee49943e45
Corbin Poteet <corbin.poteet@gmail.com>
2024-04-02 22:19:06 -0500

    Fix background color theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-blue-secondary min-h-screen ">
+      
+    </div>
   );
 }
