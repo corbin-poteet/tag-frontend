874fd38b99ff02e51e50d2b3e23362d69d72d13b
Corbin Poteet <corbin.poteet@gmail.com>
2024-04-03 13:28:51 -0500

    Rename TagService to service

--- src/app/service.tsx
+++ src/app/service.tsx
@@ -1,9 +1,7 @@
 import axios from "axios";
 import { Tag } from "@/types";
 
-const REST_API_BASE_URL = "http://localhost:8080/tag"; // @TODO: changed to /api/tags
-
-
+const REST_API_BASE_URL = "http://localhost:8080/api/v1/tags"; // @TODO: changed to /api/tags
 
 export const getTags = async () => {
   const response = await axios.get(REST_API_BASE_URL);
