f04abedb35824f03b31a998d7bf9ee7ee16edaad
Corbin Poteet <corbin.poteet@gmail.com>
2024-04-03 19:43:03 -0500

    Fix favicon defaulting to next js icon
    I THINK

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Thrive Pet Healthcare | Fullstack CRUD Application",
   description: "A simple CRUD full stack web application created for Thrive Pet Healthcare's interview project.",
+  icons: {
+    icon: {
+      url: "/favicon.ico",
+      type: "image/x-icon",
+    },
+  }
 }
+
 
 export default function RootLayout({
   children,
